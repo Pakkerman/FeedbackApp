ae622cb46d6ba22635c2fcdb3432ce68a32cc429
Pakkerman <Pakkermandev.gmail.com>
2023-01-31 16:02:17 +0800

    final

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -5,13 +5,15 @@ import FeedbackContext from '../context/FeedbackContext'
 function FeedbackStats() {
   const { feedback } = useContext(FeedbackContext)
   // Calculate Avg Rating
-  let avg =
-    feedback.reduce((prev, curr) => prev + curr.rating, 0) / feedback.length
-  avg = avg.toFixed(1).replace(/[.,]0$/, '')
+
+  const ratingSum = feedback.reduce((prev, curr) => prev + +curr.rating, 0)
+  let ratingAvg = ratingSum / feedback.length
+  ratingAvg = ratingAvg.toFixed(1).replace(/[.,]0$/, '')
+
   return (
     <div className="feedback-stats">
       <h4>{feedback.length} Reviews</h4>
-      <h4>Average Rating: {isNaN(avg) ? 0 : avg}</h4>
+      <h4>Average Rating: {isNaN(ratingAvg) ? 0 : ratingAvg}</h4>
     </div>
   )
 }
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,5 +1,4 @@
-import { v4 as uuidv4 } from 'uuid'
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 
 const FeedbackContext = createContext()
 
@@ -9,36 +8,70 @@ const FeedbackContext = createContext()
 // and provide contexts? what?
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    { id: 1, text: 'This is feedback item#1', rating: 10 },
-    { id: 2, text: 'This is feedback item#2', rating: 8 },
-    { id: 3, text: 'This is feedback item#3', rating: 7 },
-  ])
-
+  const [isLoading, setIsLoading] = useState(true)
+  const [feedback, setFeedback] = useState([])
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {}, // put the old stuff in side
     edit: false,
   })
+
+  // Populate data
+  useEffect(() => {
+    fetchFeedback()
+  }, [])
+
+  // Fetch feedback from server
+  const fetchFeedback = async () => {
+    const response = await fetch(`/feedback?_sort=id&_order=desc`)
+    const data = await response.json()
+    setFeedback(data)
+    setIsLoading(false)
+  }
+
   // Add feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newFeedback),
+    })
+
+    const data = await response.json()
+
+    setFeedback([data, ...feedback])
   }
 
   // Update feedback item
-  const updateFeedback = (id, updatedItem) => {
+  const updateFeedback = async (id, updatedItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(updatedItem),
+    })
+    console.log(JSON.stringify(updatedItem))
+    const data = await response.json()
+
+    console.log(data)
+
     setFeedback(
-      feedback.map((item) =>
-        item.id === id ? { ...item, ...updatedItem } : item
-      )
+      feedback.map((item) => {
+        console.log('spread->', { ...item, ...data })
+        console.log('NO spread->', { item, data })
+        return item.id === id ? { ...item, ...data } : item
+      })
     )
   }
 
   // Delete feedback
   const deleteFeedback = (id) => {
     if (window.confirm('are you sure?')) {
-      setFeedback(feedback.filter((item) => item.id !== id))
+      fetch(`/feedback/${id}`, {
+        method: 'DELETE',
+      })
     }
+    fetchFeedback()
   }
   // Set item to be updated
   const editFeedback = (item) => {
@@ -50,6 +83,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         feedbackEdit,
+        isLoading,
         updateFeedback,
         addFeedback,
         editFeedback,
